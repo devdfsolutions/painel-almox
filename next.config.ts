7d9260085279a874eaa966140ef1befa4935c0fc
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-29 09:38:52 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    // garante que o Vercel leve os binários do Prisma pro bundle
+    outputFileTracingIncludes: {
+      "/api/(.*)": [path.join(process.cwd(), "node_modules/.prisma/client")],
+      "/app/(.*)": [path.join(process.cwd(), "node_modules/.prisma/client")],
+    },
+  },
 };
 
 export default nextConfig;
