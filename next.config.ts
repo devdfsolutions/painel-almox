1951914f6dfc8fdfb6498419cbf2bbb9b4c3aefc
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-29 20:00:16 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import path from "path";
 
 const nextConfig = {
-  // Garante que os arquivos da pasta .prisma/client vão junto nas Serverless Functions
   outputFileTracingIncludes: {
     "/api/(.*)": [path.join(process.cwd(), "node_modules/.prisma/client/**")],
     "/app/(.*)": [path.join(process.cwd(), "node_modules/.prisma/client/**")]
