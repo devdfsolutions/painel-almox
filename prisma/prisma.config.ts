fd48322c035ae56ca2aab3ad9485aec2fb94d593
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-30 10:02:21 -0300

    Update prisma.config.ts

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,16 +1,11 @@
-// prisma.config.ts
-import type { PrismaConfig } from "prisma/config";
-
-const config: PrismaConfig = {
-  // força o client a usar o runtime que o Vercel usa (linux + openssl 3)
-  // assim o "query-engine-rhel-openssl-3.0.x" já vai pra node_modules/.prisma/client
+const config = {
   generators: [
     {
       name: "prisma-client-js",
       provider: "prisma-client-js",
-      binaryTargets: ["native", "rhel-openssl-3.0.x"],
-    },
-  ],
+      binaryTargets: ["native", "rhel-openssl-3.0.x"]
+    }
+  ]
 };
 
 export default config;
