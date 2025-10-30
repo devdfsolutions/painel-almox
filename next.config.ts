dece0cc0a70e3ddd78590db304e93567492501c3
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-29 21:44:36 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,16 @@
-// next.config.ts
 import path from "path";
 
-// Não tipar como NextConfig evita erro de TS quando chaves mudam entre versões
 const nextConfig = {
-  // Força o bundle a levar os binários do Prisma
   outputFileTracingIncludes: {
     "/api/(.*)": [
       path.join(process.cwd(), "node_modules/.prisma/client"),
-      path.join(process.cwd(), "node_modules/@prisma/client"),
+      path.join(process.cwd(), "node_modules/@prisma/client")
     ],
     "/(.*)": [
       path.join(process.cwd(), "node_modules/.prisma/client"),
-      path.join(process.cwd(), "node_modules/@prisma/client"),
-    ],
-  },
+      path.join(process.cwd(), "node_modules/@prisma/client")
+    ]
+  }
 };
 
 export default nextConfig;
