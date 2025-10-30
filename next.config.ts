df748451c5d7426f9d94f3f89fb09b505c2b7d3e
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-30 05:45:52 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    // todas as rotas de API
     "/src/app/api/(.*)": [
       path.join(process.cwd(), "node_modules/.prisma/client"),
       path.join(process.cwd(), "node_modules/@prisma/client")
     ],
-    // fallback geral (páginas que possam chamar prisma no server)
     "/(.*)": [
       path.join(process.cwd(), "node_modules/.prisma/client"),
       path.join(process.cwd(), "node_modules/@prisma/client")
