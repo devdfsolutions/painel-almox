ef4e5515437affa4d7474b6bc33be2c44bc93fca
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-30 07:07:16 -0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,8 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-export const runtime = "nodejs";        // garante que não vai pra edge
-export const dynamic = "force-dynamic"; // sem cache
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
