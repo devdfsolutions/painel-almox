834637b26a085cd003e8975817cdb96579b44fbb
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-30 08:01:31 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,13 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  outputFileTracing: true,
   outputFileTracingIncludes: {
     "/api/(.*)": [
       path.join(process.cwd(), "node_modules/.prisma/client"),
       path.join(process.cwd(), "node_modules/@prisma/client")
-    ]
-  }
+    ],
+  },
 };
 
 export default nextConfig;
