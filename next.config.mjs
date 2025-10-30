285a318d9266b67a9a39af545befb80a269ab3b7
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-30 05:31:22 -0300

    Update and rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
+// next.config.mjs
 import path from "path";
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {},
+  // isso aqui manda o Vercel levar a pasta dos engines do Prisma
   outputFileTracingIncludes: {
     "/api/(.*)": [
       path.join(process.cwd(), "node_modules/.prisma/client"),
